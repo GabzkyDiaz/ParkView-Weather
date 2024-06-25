8ba60badd54e77c7234ae03dc6a371bff62aeda3
GabzbyDiaz <gabzky.diaz@gmail.com>
2024-06-24 22:09:04 -0500

    added hierarchical search

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,7 +1,12 @@
 class ParksController < ApplicationController
   def index
-    if params[:search]
+    @activities = Activity.all
+    if params[:search] && params[:activity_id]
+      @parks = Park.joins(:activities).where('parks.name LIKE ? AND activities.id = ?', "%#{params[:search]}%", params[:activity_id]).page(params[:page])
+    elsif params[:search]
       @parks = Park.where('name LIKE ?', "%#{params[:search]}%").page(params[:page])
+    elsif params[:activity_id]
+      @parks = Park.joins(:activities).where('activities.id = ?', params[:activity_id]).page(params[:page])
     else
       @parks = Park.page(params[:page])
     end
